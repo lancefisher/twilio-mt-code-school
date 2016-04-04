0435efe2e366efe0298dbe0cdc7fc6a0ebb35aab
Lance Fisher <lance@submittable.com>
2016-04-04 15:35:17 -0600

    Send an image with an SMS

--- sms.js
+++ sms.js
@@ -5,7 +5,8 @@ var twilio = require('twilio')(config.twilio.accountSid, config.twilio.authToken
 twilio.messages.create({
   body: 'hello from twilio!',
   to: config.myPhone,
-  from: config.twilio.from
+  from: config.twilio.from,
+  mediaUrl: 'http://www.placecage.com/200/300'
 }, function createCb(err, data) {
   if (err) return console.log('error sending message!', err);
   return console.log('sent message', data);
