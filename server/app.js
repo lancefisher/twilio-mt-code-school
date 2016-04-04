9f70a7de1d89e78c0b89e3169007f5f0b138e1e5
Lance Fisher <lance@submittable.com>
2016-04-04 17:00:47 -0600

    Send reply SMS

--- server/app.js
+++ server/app.js
@@ -2,9 +2,18 @@
 var express = require('express');
 var app = new express.Router();
 
-app.get('/sms', function getCb(req, res) {
-  console.log('REQUEST to /sms');
-  res.send({ ok: true });
+app.post('/sms', function getCb(req, res) {
+  console.log(req.body);
+
+  var twiml = [
+    '<?xml version="1.0" encoding="UTF-8" ?>',
+    '<Response>',
+    '  <Message>You said: ' + req.body.Body + '</Message>',
+    '</Response>'
+  ].join('');
+
+  res.set('Content-Type', 'text/xml');
+  res.send(twiml);
 });
 
 module.exports = app;
